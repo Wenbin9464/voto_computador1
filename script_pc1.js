668a3a7e0e4940e8418f3813a88387b172301cfe
Wenbin9464 <wenbin.liang@escola.pr.gov.br>
2025-08-18 20:02:56 -0300

    Update script_pc1.js
    adicionar som função

--- script_pc1.js
+++ script_pc1.js
@@ -554,6 +554,12 @@ function confirmVote(chapa){
       document.getElementById("login-error").textContent="";
     },500);
   }
+  tocarSom();
+}
+function tocarSom() {
+  const audio = document.getElementById("som");
+  audio.currentTime = 0; // Reinicia o áudio se já estiver tocando
+  audio.play();
 }
 
 // ======== Resultados ========
